1cf3d84ffebb020f98df9f51be2078980c7df0d5
eubelts <eunicesausbeltran@yahoo.com>
2018-11-05 15:22:48 +0800

    Initial commit

---
+++ lib/stretchy.rb
@@ -0,0 +1,77 @@
+require 'json'
+require 'logger'
+require 'forwardable'
+require 'elasticsearch'
+
+require 'stretchy/api'
+require 'stretchy/utils'
+require 'stretchy/version'
+require 'stretchy/and_collector'
+
+module Stretchy
+
+  module_function
+
+  def client
+   @client ||= Elasticsearch::Client.new
+ end
+
+ def client=(client)
+    @client = client
+  end
+
+  def search(options = {})
+    client.search(options)
+  rescue Elasticsearch::Transport::Transport::Errors::BadRequest => bre
+    msg = bre.message[-150..-1]
+    msg << "\n\n"
+    msg << JSON.pretty_generate(options)
+    raise msg
+  end
+
+  def index_exists?(name)
+    client.indices.exists? index: name
+  end
+
+  def delete_index(name)
+    client.indices.delete(index: name) if index_exists? name
+  end
+
+  def create_index(name, params = {})
+    client.indices.create({index: name}.merge(params)) unless index_exists? name
+  end
+
+  def refresh_index(name, params = {})
+    client.indices.refresh({index: name}.merge(params)) if index_exists? name
+  end
+
+  def count_index(name, params = {})
+    client.count({index: name}.merge(params))['count']
+  end
+
+  def index_document(params = {})
+    Utils.require_params!(:index_document, params, :index, :type, :body)
+
+    raise IndexDoesNotExistError.new(
+      "index #{params[:index]} does not exist"
+    ) unless index_exists? params[:index]
+
+    client.index(params)
+  end
+
+  def query(options = {})
+    API.new(root: options)
+  end
+  #
+  # def fake_results
+  #   Results.fake
+  # end
+
+  # def method_missing(method, *args, &block)
+  #   if client.respond_to?(method)
+  #     client.send(method, *args, &block)
+  #   else
+  #     super
+  #   end
+  # end
+end
---
+++ stretchy.gemspec
@@ -0,0 +1,37 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'stretchy/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "stretchy"
+  spec.version       = Stretchy::VERSION
+  spec.authors       = ["eubelts"]
+  spec.email         = ["eunicesausbeltran@yahoo.com"]
+
+  if spec.respond_to?(:metadata)
+    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com' to prevent pushes to rubygems.org, or delete to allow pushes to any server."
+  end
+
+  spec.summary       = %q{Query builder for Elasticsearch}
+  spec.description   = %q{Build queries for Elasticsearch with a chainable interface like ActiveRecord's.}
+  # spec.homepage      = "Put your gem's website or public repo URL here."
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.bindir        = "exe"
+  spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.8"
+  spec.add_development_dependency "rake", "~> 10.0"
+  
+  spec.add_dependency "elasticsearch",  "~> 5.0"
+
+  spec.add_development_dependency "rspec",          "~> 3.2"
+  spec.add_development_dependency "fuubar",         "~> 2.0"
+  spec.add_development_dependency "pry",            "~> 0.10"
+  spec.add_development_dependency "awesome_print",  "~> 1.6"
+  spec.add_development_dependency "yard",           "~> 0.9"
+
+end
